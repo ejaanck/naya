e67d37ead8a693973f2c54a7849a5db39b7524e9
ejaanck <149357949+ejaanck@users.noreply.github.com>
2023-12-27 03:07:10 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/naya1503/Naya-Userbot.git"
-DIR="/root/naya1503"
+REPO="https://github.com/ejaanck/naya.git"
+DIR="/root/ejaanck"
 
 spinner(){
     local pid=$!
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Naya-Userbot ${BRANCH}... "
+    echo -e "Cloning naya ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
